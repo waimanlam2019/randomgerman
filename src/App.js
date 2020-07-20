96a846280b2dcd7d07f233878f8332adf79ec580
Ray Lam <gunbuster363@gmail.com>
2020-07-20 13:52:20 +0800

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.generateRandomGerman = this.generateRandomGerman.bind(this);
+    this.createStringFromObject = this.createStringFromObject.bind(this);
+    this.state = { german: "", display: false };
+  }
+
+  createStringFromObject(obj) {
+    return obj.subject + " " + obj.verb + " " + obj.object + ".";
+  }
+
+  generateRandomGerman() {
+    fetch("https://beliebigersatz.herokuapp.com/sentence")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let sentence = this.createStringFromObject(data);
+        this.setState({ german: sentence, display: true });
+      });
+
+    //var realSentence = this.createStringFromObject(sentence);
+
+    //this.setState({german: sentence, display:true});
+  }
+
+  render() {
+    const randomgerman = this.state.german;
+    const display = this.state.display;
+    return (
+      <div>
+        <DisplayArea display={display} german={randomgerman} />
+        <GenerateButton generateRandomGerman={this.generateRandomGerman} />
+      </div>
+    );
+  }
+}
+
+class DisplayArea extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const randomgerman = this.props.german;
+    const display = this.props.display;
+    if (display) {
+      return <div className="text">{randomgerman}</div>;
+    }
+
+    return <div className="text">Random Text Here</div>;
+  }
+}
+
+class GenerateButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.generateRandomGerman();
+  }
+
+  render() {
+    return (
+      <button
+        id="generateBtn"
+        type="button"
+        className="btn btn-outline-primary btn-lg btn-block"
+        onClick={this.handleClick}
+      >
+        Generate Random German
+      </button>
+    );
+  }
 }
 
 export default App;
